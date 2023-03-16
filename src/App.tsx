879c30c56fb60dff6b674d62884be069021d6564
Colby Birkhead <birkheadc@gmail.com>
2023-03-16 20:08:14 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './App.css'
+
+interface IAppProps {
+
+}
+
+function App(props: IAppProps): JSX.Element {
+  return (
+    <div id='client-wrapper'>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const path = require('path');
+const DotenvWebpackPlugin = require('dotenv-webpack');
+
+module.exports = {
+  entry: './index.tsx',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/'
+  },
+  target: 'web',
+  devServer: {
+    host: '0.0.0.0',
+    port: '3000',
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    },
+    allowedHosts: ['all'],
+    static: {
+      directory: path.join(__dirname, 'public')
+    },
+    open: false,
+    hot: true,
+    liveReload: true,
+    historyApiFallback: true
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    fallback: {
+      "fs": false
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html')
+    }),
+    new NodePolyfillPlugin(),
+    new Dotenv({
+      systemvars: true
+    })
+  ]
+}
